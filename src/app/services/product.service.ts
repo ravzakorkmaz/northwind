0d239da133d6e7bba61911940931b5ec84c4533e
rvzko <ravza.korkmaz@rub.de>
2021-03-13 21:53:35 +0100

    commit 1

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { listResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl = 'https://localhost:44324/api/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts():Observable<listResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<listResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<listResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId;
+    return this.httpClient.get<listResponseModel<Product>>(newPath);
+  }
+
+}
